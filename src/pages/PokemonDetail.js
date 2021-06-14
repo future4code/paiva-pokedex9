a95ec0ef1f1f88d1545efc7d4eae220a79b5d02a
Renato Marinho <renatomarinho@pop-os.localdomain>
2021-06-14 14:35:26 -0300

    rotas criadas

---
+++ src/pages/PokemonDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function PokemonDetail() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+  
+  export default PokemonDetail;
+  
