e71297cfb7a448cdd1b2c5c46585aa36c2e307c2
Renato Marinho <renato.marinhofr@gmail.com>
2021-06-17 11:46:04 -0300

    detail pokemon

--- src/hooks/UseRequestDetail.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import { useState, useEffect } from "react";
-import url_Base from '../Url/Url'
-import {useParams} from 'react-router-dom'
-
-
-
-export default UseRequestDetail;
--- src/pages/PokemonDetail.js
+++ src/pages/PokemonDetail.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { Progress, Stack, Text, Tab } from "@chakra-ui/react";
+import { Progress, Stack, Text, Flex } from "@chakra-ui/react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { BASE_URL } from "../Url/BASE_URL";
 import { useParams } from "react-router-dom";
+import useRequestDetail from "../hooks/useRequestDetail";
 
 const DetailsContainer = styled.div`
   display: grid;
@@ -54,107 +55,65 @@ const Title = styled.h1`
 `;
 
 const PokemonDetail = () => {
-  const [DetailPoke, setDetailPoke] = useState({
-    habilidades: {},
-    ataques: {},
-    peso: "",
-    tipo: {},
-  });
-
   const params = useParams();
+  const detailPoke = useRequestDetail([], `${BASE_URL}/${params.name}`);
 
-  useEffect(() => {
-    axios
-      .get(`${BASE_URL}/${params.name}`)
-      .then((res) => {
-        setDetailPoke.habilidades(res.data.abilities.ability);
-      })
-      .catch((erro) => {
-        console.log(erro);
-      });
-  });
+  console.log("DETALHES", detailPoke.stats);
+
+  const poderes =
+    detailPoke.stats &&
+    detailPoke.stats.map((stat) => {
+      return (
+        <>
+          <h3>
+            <p>{stat.stat.name}</p>
+            <p>{stat.base_stat}</p>
+          </h3>
+          <Progress colorScheme="red" size="sm" max={150} value={stat.base_stat} />
+        </>
+      );
+    });
+
+  const ataques =
+    detailPoke.moves &&
+    detailPoke.moves.map((ataque, index) => {
+      return (
+        index < 5 && (
+          <Text key={ataque.move.name} fontSize={16} fontWeight="bold">
+            {ataque.move.name}
+          </Text>
+        )
+      );
+    });
 
   return (
-    <DetailsContainer>
-      {console.log(DetailPoke.habilidades)}
-      <DetailsCard1>
-        <Title>Bulbasaur</Title>
-        <img
-          src={
-            "https://raw.githubusercontent.com/pokeAPI/sprites/master/sprites/pokemon/1.png"
-          }
-        />
-        <Text fontWeight="bold">Tipo: grama</Text>
-      </DetailsCard1>
-      <Stack spacing={2} justify="center">
-        <Title>Poderes</Title>
-        <h3>
-          <p>hp</p>
-          <p>80</p>
-        </h3>
-        <Progress colorScheme="red" size="sm" value={80} />
-        <h3>
-          <p>attack</p>
-          <p>82</p>
-        </h3>
-        <Progress colorScheme="red" size="sm" value={82} />
-        <h3>
-          <p>defense</p>
-          <p>83</p>
-        </h3>
-        <Progress colorScheme="red" size="sm" value={83} />
-        <h3>
-          <p>special-attack</p>
-          <p>100</p>
-        </h3>
-        <Progress colorScheme="red" size="sm" value={100} />
-        <h3>
-          <p>special-defense</p>
-          <p>100</p>
-        </h3>
-        <Progress colorScheme="red" size="sm" value={100} />
-        <h3>
-          <p>speed</p>
-          <p>80</p>
-        </h3>
-        <Progress colorScheme="red" size="sm" value={80} />
-      </Stack>
-      <DetailsCard2>
-        <Title>Ataques</Title>
-        <br />
-        <br />
-        <Text fontSize={16} fontWeight="bold">
-          swords-dance
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          cut
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          bind
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          vine-whip
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          headbutt
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          tackle
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          body-slam
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          take-down
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          double-edge
-        </Text>
-        <Text fontSize={16} fontWeight="bold">
-          growl
-        </Text>
-      </DetailsCard2>
-    </DetailsContainer>
+    <>
+      {detailPoke && detailPoke.sprites ? (
+        <DetailsContainer>
+          <DetailsCard1>
+            <Title>{params.name}</Title>
+            <img src={detailPoke.sprites.front_default} />
+            <Text fontWeight="bold">Tipo:</Text>
+            {detailPoke &&
+              detailPoke.types.map((type) => {
+                return <Text key={type.type.name}>{type.type.name}</Text>;
+              })}
+          </DetailsCard1>
+          <div>{poderes}</div>
+          <DetailsCard2>
+            <Title>Ataques</Title>
+            <br />
+            <br />
+            <Stack spacing={2} justify="center">
+              <Title>Poderes</Title>
+              {ataques}
+            </Stack>
+          </DetailsCard2>
+        </DetailsContainer>
+      ) : (
+        <p>Carregando informações</p>
+      )}
+    </>
   );
 };
 export default PokemonDetail;
