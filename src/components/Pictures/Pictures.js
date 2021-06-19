b31a21c88d0042aa6b839fbcaa6a2a47bf194e29
bruxxi <bruna_nunciato@hotmail.com>
2021-06-19 15:26:52 -0300

    ok

--- src/components/Pictures/Pictures.js
+++ src/components/Pictures/Pictures.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { useState, useEffect } from "react";
 import {useParams} from 'react-router-dom'
-import Header from "../Header/Header";
 import {
   Container,
   CircleBlue,
