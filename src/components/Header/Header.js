35d3ed67d8089cbfe03105e62e33056622a946cf
bruxxi <bruna_nunciato@hotmail.com>
2021-06-20 13:45:50 -0300

    condicional pagina

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,22 @@ import { HeaderContainer, BallContainer, LogoContainer } from "./style";
 import PokemonLogo from "../../img/pokemon-logo.png";
 import Pokeball from "../../img/pokeball.gif";
 import { Link } from "react-router-dom";
+import { useHistory } from 'react-router-dom'
 
 const Header = () => {
+  const history = useHistory()
+
   return (
     <HeaderContainer>
       <Link to="/">
         <LogoContainer src={PokemonLogo} alt="Logo Pokemon" />
       </Link>
-      <BallContainer>
-        <Link to="/pokedex">
+      <BallContainer> 
+        {history.location.pathname === "/pokedex" ? <p></p> : <Link to="/pokedex">
           <img src={Pokeball} alt="imagem pokebola" />
         </Link>
+        }
+        
       </BallContainer>
     </HeaderContainer>
   );
--- src/pages/Pokedex/style.js
+++ src/pages/Pokedex/style.js
@@ -1,57 +1,55 @@
 import styled from 'styled-components'
 
-export const PokedexContainer= styled.div`
-background-color: rgb(238, 215, 75);
-height: 100vh;
-
-`
+export const PokedexContainer = styled.div`
+  background-color: rgb(238, 215, 75);
+  height: 100vh;
+`;
 export const BattleContainer = styled.div`
-display: flex;
-flex-direction: column;
-> div {
-display: flex;
-align-items: center;
-justify-content: center;
-width: 300px;
-align-self: center;
+  display: flex;
+  flex-direction: column;
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 300px;
+    align-self: center;
 
- > div {
-     display: flex;
-     align-items: center;
-     justify-content: center;
-     flex-direction: column;
-     margin: 0 18px;
- }
- h1 {
-     font-family: 'pokeHollow';
- }
-img {
-    width: 70px;
-    height: 70px;
-    background-color: white;
-    border-radius: 5px;
-}
-h2 {
-    text-transform: capitalize;
-}
-}
-#winner {
+    > div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      margin: 0 18px;
+    }
+    h1 {
+      font-family: "pokeHollow";
+    }
+    img {
+      width: 70px;
+      height: 70px;
+      background-color: white;
+      border-radius: 5px;
+    }
+    h2 {
+      text-transform: capitalize;
+    }
+  }
+  #winner {
     width: 100%;
     text-align: center;
-    text-transform:capitalize;
-}
-
-`
-export const PokeImage= styled.img`
-width: 50px;
-height: 50px;
-`
+    text-transform: capitalize;
+  }
+`;
+export const PokeImage = styled.img`
+  width: 50px;
+  height: 50px;
+`;
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   gap: 2%;
-  color : black;
-`
+  color: black;
+`;
 
 export const Title = styled.h1`
   margin-top: 20px;
